826693b5b3e05186fa4e4ff94b6d16df894ff63c
Sheng-Loong Su <sushengloong@gmail.com>
2014-07-27 21:44:10 +0800

    limit reddit search results to 10

--- app/models/reddit_client.rb
+++ app/models/reddit_client.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 
 class RedditClient
   def search q
-    resp = JSON.parse open( URI.escape("http://www.reddit.com/search.json?q=#{q}") ).read
+    resp = JSON.parse open( URI.escape("http://www.reddit.com/search.json?q=#{q}&limit=10") ).read
     resp["data"]["children"].map do |item|
       data = item["data"]
       # cannot find thumbnail link from the API so we use Reddit open source thumbnail
